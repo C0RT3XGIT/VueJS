172abd765f3e540bc8f92f515bf47ff9d83b0062
C0RT3XGIT <71043631+C0RT3XGIT@users.noreply.github.com>
2021-04-29 11:57:45 +0300

    Final commit

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -16,7 +16,7 @@
 
     <nav class="green darken-2">
       <div class="nav-wrapper">
-        <router-link to="/" class="brand-logo">Task</router-link>
+        <router-link to="/" class="brand-logo">Tasks</router-link>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
           <router-link
               v-for="(link,i) in this.$router.options.routes"
@@ -32,7 +32,7 @@
           <li>
             <a class="dropdown-trigger" ref="dropdowntrigger" href="#!" data-target="dropdown1">API Json<i
                 class="material-icons right">arrow_drop_down</i></a>
-          </li>sd
+          </li>
         </ul>
       </div>
     </nav>
